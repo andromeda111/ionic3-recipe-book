d75690247734795b18bf0681be689cef46d2eeb2
andromeda111 <jmseymour111@gmail.com>
2017-12-10 13:44:41 -0800

    Added signup and signin pages

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
 
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html',
 })
 export class SigninPage {
+
+  onSignin(form: NgForm) {
+    console.log(form.value);
+    
+  }
+
 
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
 
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+
+  onSignup(form: NgForm) {
+    console.log(form.value);
+    
+  }
+
 
 }
