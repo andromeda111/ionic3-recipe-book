c60591f50da063a6d877861bef5292c6be6d9098
John <jmseymour111@gmail.com>
2017-12-24 11:03:09 -0800

    set version for firebase in package json, implemented auth logout and page restriction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,36 @@ import { SigninPage } from '../pages/signin/signin';
 import { SignupPage } from '../pages/signup/signup';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
 import firebase from "firebase";
+import { AuthService } from '../services/auth';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  tabsPage:any = TabsPage;
+  rootPage:any = TabsPage;
   signinPage: any = SigninPage;
   signupPage: any = SignupPage;
+  isAuthenticated: boolean = false;
 
   @ViewChild('nav') nav: NavController;
 
   constructor(platform: Platform,
               statusBar: StatusBar,
               splashScreen: SplashScreen,
-              private menuCtrl: MenuController) {
+              private menuCtrl: MenuController,
+              private authService: AuthService) {
     firebase.initializeApp({
       apiKey: "AIzaSyCApx5KZzlkibGzxafdFbncfGHcMyt2G6I",
       authDomain: "ionic2-recipe-book-82ac5.firebaseapp.com",
+    })
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.isAuthenticated = true;
+        this.rootPage = TabsPage;
+      } else {
+        this.isAuthenticated = false;
+        this.rootPage = SigninPage;
+      }
     })
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -40,7 +52,9 @@ export class MyApp {
   }
 
   onLogout() {
-
+    this.authService.logout();
+    this.menuCtrl.close();
+    this.nav.setRoot(SigninPage);
   }
 }
 
